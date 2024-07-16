f5260bd2b9f55839019e6a09473cce31df85a072
James-Cochran <jameslikesfruit@gmail.com>
2024-07-16 08:13:13 -0400

    added has_many and belongs_to relationships to recipe, recipie_ingredient, and ingredient models.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,5 @@
 class Recipe < ApplicationRecord
    validates :name, :complexity, :genre, presence: true
+   has_many :recipe_ingredients
+   has_many :ingredients, through: :recipe_ingredients
 end
