f41d800162b29df55a0ba32aabcb795a88d4bf96
James-Cochran <jameslikesfruit@gmail.com>
2024-07-16 09:31:22 -0400

    recipes show spec file and test for recipe's show page.

---
+++ spec/features/recipes_show_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe "recipes show" do
+
+    before(:each) do
+        @recipe_1 = Recipe.create!(name: "Hamburger", complexity: 1, genre: "easy")
+        @recipe_2 = Recipe.create!(name: "Fries", complexity: 2, genre: "fried")
+        @ingredient_1 = Ingredient.create!(name: "Ground Beef", cost: 2)
+        @ingredient_2 = Ingredient.create!(name: "Salt", cost: 4)
+        @ingredient_3 = Ingredient.create!(name: "Potato", cost: 5)
+
+        @recipe_1.ingredients << [@ingredient_1]
+        @recipe_2.ingredients << [@ingredient_2, @ingredient_3]
+    end
+
+    describe "when I visit show page" do
+       it "displays the recipe's name, complexity, genre, and ingredient names" do
+        
+            visit "/recipes/#{@recipe_2.id}"
+
+            expect(page).to have_content(@recipe_2.name)
+            expect(page).to have_content(@recipe_2.complexity)
+            expect(page).to have_content(@recipe_2.genre)
+
+            expect(page).to have_content(@ingredient_2.name)
+            expect(page).to_not have_content(@ingredient_1.name)
+       end
+    end
+end
